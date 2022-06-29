5f7fb793390281534ed591e6c0a5e2916ba0876c
Septiana Yoga <septianayoga30@gmail.com>
2022-06-29 11:23:26 +0700

    initial code for hero section

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-left-side">Left</div>
+      <div className="hero-right-side">Right</div>
+    </div>
+  );
+};
+
+export default Hero;
