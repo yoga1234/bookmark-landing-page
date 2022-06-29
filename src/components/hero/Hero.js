28a82305b7190354b99ed7554bd360e23ddabd09
Septiana Yoga <septianayoga30@gmail.com>
2022-06-29 14:18:22 +0700

    adding basic style to right side

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,12 +1,30 @@
 import React from "react";
 
 import "./Hero.css";
+import heroImg from "../../assets/illustration-hero.svg";
 
 const Hero = () => {
   return (
     <div className="hero-container">
-      <div className="hero-left-side">Left</div>
-      <div className="hero-right-side">Right</div>
+      <div className="hero-left-side">
+        <h2>A Simple Bookmark Manager</h2>
+        <p>
+          A clean and simple interface to organize your favorite websites. Open
+          a new browser tab and see your sites load instantly. Try it for free.
+        </p>
+        <div className="hero-button-container">
+          <button className="hero-button hero-chrome-btn">
+            Get it on Chrome
+          </button>
+          <button className="hero-button hero-firefox-btn">
+            Get it on Firefox
+          </button>
+        </div>
+      </div>
+      <div className="hero-right-side">
+        <img src={heroImg} alt="Browser Tab" />
+        <div className="blue-hero-block"></div>
+      </div>
     </div>
   );
 };
