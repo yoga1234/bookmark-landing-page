825e24d3d873e2017be600106ce0cd135dcf6a89
Septiana Yoga <septianayoga30@gmail.com>
2022-06-30 11:03:11 +0700

    adding features section

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import "./Features.css";
+
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="features-container">
+      <h2>Features</h2>
+      <p>
+        Our aim is to make it quick and easy for you to access your favourite
+        websites. Your bookmarks sync between your devices so you can access
+        them on the go.
+      </p>
+    </div>
+  );
 };
 
 export default Features;
