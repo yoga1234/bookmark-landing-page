c453b064099973c84911b2bb73ff1c83b9d6559e
Septiana Yoga <septianayoga30@gmail.com>
2022-07-02 13:34:29 +0700

    initial code for download extension

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Navbar, Hero, Features, FeatureTabs } from "./components";
+import DownloadExtension from "./components/downloadExtension/DownloadExtension";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Hero />
       <Features />
       <FeatureTabs />
+      <DownloadExtension />
     </div>
   );
 }
---
+++ src/components/downloadExtension/DownloadExtension.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DownloadExtension = () => {
+  return <div>DownloadExtension</div>;
+};
+
+export default DownloadExtension;
