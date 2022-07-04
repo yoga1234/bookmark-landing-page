3aaef2c12d433ed508c227b9a092c7b89cd71409
Septiana Yoga <septianayoga30@gmail.com>
2022-07-04 11:56:46 +0700

    adding structure code for faqs

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Faq = () => {
-  return <div>Faq</div>;
+  return (
+    <div className="faq-container">
+      <div className="faq-heading-container">
+        <h3>Frequently Asked Questions</h3>
+        <p>
+          Here are some of our FAQs. If you have any other questions you'd like
+          answered please feel free to email us.
+        </p>
+      </div>
+      <div className="faq-content-dropdown"></div>
+    </div>
+  );
 };
 
 export default Faq;
