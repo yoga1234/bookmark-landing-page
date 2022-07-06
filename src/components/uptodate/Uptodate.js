66d105dbeb076f1b2e2cd4ca80ea0a87994d86c5
Septiana Yoga <septianayoga30@gmail.com>
2022-07-06 16:44:18 +0700

    initial styles for uptodate

--- src/components/uptodate/Uptodate.js
+++ src/components/uptodate/Uptodate.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import "./Uptodate.css";
+
 const Uptodate = () => {
-  return <div>Uptodate</div>;
+  return (
+    <div className="uptodate-container">
+      <p>35.000+ ALREADY JOINED</p>
+      <h3>Stay up-to-date with what we're doing</h3>
+      <div className="form-mail">
+        <input type="text" />
+        <button>Contact Us</button>
+      </div>
+    </div>
+  );
 };
 
 export default Uptodate;
