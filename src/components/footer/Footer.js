d39306be4b9566bda68e2b65f325305796e2db67
Septiana Yoga <septianayoga30@gmail.com>
2022-07-11 15:07:17 +0700

    responsiveness for 4k display

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,15 +6,17 @@ import { logoBookmarkWhite, iconFacebook, iconTwitter } from "../../assets";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <div className="footer-left">
-        <img src={logoBookmarkWhite} alt="Bookmark" />
-        <p>FEATURES</p>
-        <p>PRICING</p>
-        <p>CONTACT</p>
-      </div>
-      <div className="footer-right">
-        <img src={iconFacebook} alt="Facebook" />
-        <img src={iconTwitter} alt="Twitter" />
+      <div className="footer-width">
+        <div className="footer-left">
+          <img src={logoBookmarkWhite} alt="Bookmark" />
+          <p>FEATURES</p>
+          <p>PRICING</p>
+          <p>CONTACT</p>
+        </div>
+        <div className="footer-right">
+          <img src={iconFacebook} alt="Facebook" />
+          <img src={iconTwitter} alt="Twitter" />
+        </div>
       </div>
     </div>
   );
